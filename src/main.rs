c3525e98e1b8a541c74c9a9361c375a07daa8e38
T4D4 .icp <t4d4.icp@gmail.com>
2024-12-18 05:11:30 +0000

    メモリ機能の追加と出力表示のリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io::stdin;
 
 fn main() {
+    let mut memory: f64 = 0.0;
+    let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
         let line = line.unwrap();
@@ -9,9 +11,27 @@ fn main() {
         }
         // 空白で分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
+        // メモリへの書き込み
+        if tokens[0] == "mem+" {
+            memory += prev_result;
+            print_output(memory);
+            continue;
+        } else if tokens[0] == "mem-" {
+            memory -= prev_result;
+            print_output(memory);
+            continue;
+        }
         // 式の計算
-        let left: f64 = tokens[0].parse().unwrap();
-        let right: f64 = tokens[2].parse().unwrap();
+        let left: f64 = if tokens[0] == "mem" {
+            memory
+        } else {
+            tokens[0].parse().unwrap()
+        };
+        let right: f64 = if tokens[2] == "mem" {
+            memory
+        } else {
+            tokens[2].parse().unwrap()
+        };
         let result = match tokens[1] {
             "+" => left + right,
             "-" => left - right,
@@ -23,6 +43,12 @@ fn main() {
             }
         };
         // 結果を表示
-        println!(" => {}", result);
+        print_output(result);
+
+        prev_result = result;
     }
+}
+
+fn print_output(value: f64) {
+    println!(" => {}", value);
 }
