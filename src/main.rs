6befc00f28f3a8bd430a38ccaa65cea574a50d55
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2025-01-01 23:46:17 +0900

    メモリ機能の拡張：複数のメモリスロットを追加し、メモリ操作を関数化

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memory: f64 = 0.0;
+    let mut memories: Vec<f64> = vec![0.0; 10];
     let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
@@ -12,18 +12,17 @@ fn main() {
         // 空白で分割
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
         // メモリへの書き込み
-        if tokens[0] == "mem+" {
-            memory += prev_result;
-            print_output(memory);
+        let is_memory = tokens[0].starts_with("mem");
+        if is_memory && tokens[0].ends_with('+') {
+            add_and_print_memory(&mut memories, tokens[0], prev_result);
             continue;
-        } else if tokens[0] == "mem-" {
-            memory -= prev_result;
-            print_output(memory);
+        } else if is_memory && tokens[0].ends_with('-') {
+            add_and_print_memory(&mut memories, tokens[0], -prev_result);
             continue;
         }
         // 式の計算
-        let left: f64 = eval_token(tokens[0], memory);
-        let right: f64 = eval_token(tokens[2], memory);
+        let left = eval_token(tokens[0], memories);
+        let right = eval_token(tokens[2], memories);
         let result = eval_expression(left, tokens[1], right);
         // 結果を表示
         print_output(result);
@@ -36,9 +35,10 @@ fn print_output(value: f64) {
     println!(" => {}", value);
 }
 
-fn eval_token(token: &str, memory: f64) -> f64 {
-    if token == "mem" {
-        memory
+fn eval_token(token: &str, memories: Vec<f64>) -> f64 {
+    if token.starts_with("mem") {
+        let slot_index: usize = token[3..].parse().unwrap();
+        memories[slot_index]
     } else {
         token.parse().unwrap()
     }
@@ -55,4 +55,10 @@ fn eval_expression(left: f64, token: &str, right: f64) -> f64 {
             unreachable!();
         }
     }
+}
+
+fn add_and_print_memory(memories: &mut Vec<f64>, token: &str, prev_result: f64) {
+    let slot_index: usize = token[3..token.len() -1].parse().unwrap();
+    memories[slot_index] += prev_result;
+    print_output(memories[slot_index]);
 }
